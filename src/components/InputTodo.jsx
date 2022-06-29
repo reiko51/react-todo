4fc77ec63b5175be07ae00f165c082fdbaf8aa3a
reiko51 <71922155+reiko51@users.noreply.github.com>
2022-06-29 11:55:54 +0900

    カイゼン(TODOの上限設定)

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -11,18 +11,21 @@ const style = {
   borderRadius: "8px"
 };
 export const InputTodo = (props) => {
-  const { todoText, onChange, onClick } = props;
+  const { todoText, onChange, onClick, disabled } = props;
   return (
     // <div className="input-area">
     <div style={style}>
       <input
+        disabled={disabled}
         placeholder="TODOを入力"
         value={todoText}
         // onChange={onChangeTodoText}
         onChange={onChange}
       />
       {/* <button onClick={onClickAdd}>追加</button> */}
-      <button onClick={onClick}>追加</button>
+      <button disabled={disabled} onClick={onClick}>
+        追加
+      </button>
     </div>
   );
 };
