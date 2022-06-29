64779335f134dfdf41d8259b8d76c670ed8f2d25
reiko51 <71922155+reiko51@users.noreply.github.com>
2022-06-29 11:39:16 +0900

    カイゼン(CSS-in-JS)

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 
+// コンポーネント化するとCSSもJSX内部に書くことがある
+// オブジェクトなので各styleの最後は;ではなくて,
+const style = {
+  backgroundColor: "#c1ffff",
+  width: "400px",
+  height: "30px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "8px"
+};
 export const InputTodo = (props) => {
   const { todoText, onChange, onClick } = props;
   return (
-    <div className="input-area">
+    // <div className="input-area">
+    <div style={style}>
       <input
         placeholder="TODOを入力"
         value={todoText}
